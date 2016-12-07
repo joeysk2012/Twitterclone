82e85fae9a79997071278a27ef99987a920e9f33
joeysk2012 <kurokaw1@gmail.com>
2016-12-06 21:41:55 -0600

    first commit

---
+++ meowscript.js
@@ -0,0 +1,38 @@
+
+function makeaPost() {
+	var mybuttons = document.get.Type("Submit"); 
+	mybutton.Onclick =createNewDiv(){ 
+    var d = document.createElement('div'); //how do I do this???
+    d.classname = "meowWrapper";
+    var i = document.createElement('img')
+    i.classname = "avatar";
+    i.src = "gft/cat1.jpg";
+    var s1 = document.createElement('span')
+    s1.classname = "name" 
+    var s2 = document.createElement('span')
+    s2.classname ="time"
+    var p = document.createElement('p')
+    var text = document.get.ElementbyClass("meowText").value;
+
+    var body = docmuent.get.ElementbyClass("panel left");
+    document.body.appendChild (d);
+    document.d.appendchild(i);
+    document.d.appendchild(s1);
+    document.d.appendchild(s2);
+    document.d.appendchild(p);
+    document.p.innerHTML = "text"
+    document s1.innerHTMl = "KeKe"
+    document.s2.innerHTMl = "Now"
+    document.d.innerHTML = "kekethecat"
+}
+}
+
+Window.Onload=function(){makeaPost();}
+
+//sample coding
+				//<div class="meowWrapper">
+					//<img class="avatar" src="gfx/cat2.jpg">
+					//<span class="name">B-ray</span> @bchan <span class="time">15m</span>
+					//<p>
+						//Scratch paw, lick, explore, sleep 
+					//</p> 
